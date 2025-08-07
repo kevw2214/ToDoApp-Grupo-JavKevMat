a4b4d692627fcf20523917153b501079e9d5a551
kevin cerpa <kevindavidcerpa@gmail.com>
2025-08-07 10:02:35 -0500

    Añadir funcionalidad para marcar tareas como completadas

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#main.py
+from task_model import TaskModel
+
 def main():
-    print("Bienvenido a ToDoApp")
+    task = TaskModel("Estudiar para el examen")
+    print(f"Tarea creada: {task.get_task_name()}")
+    task.mark_as_completed()
+    print(f"Tarea completada: {task.is_completed()}")
 
 if __name__ == "__main__":
     main()
