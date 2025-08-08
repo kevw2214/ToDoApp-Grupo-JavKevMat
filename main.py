e72cdcc9d67333d6266453e1938d3114b1486f78
MatteOZz12 <mateozambrgonza@gmail.com>
2025-08-07 20:35:28 -0500

    Añadir set_done y remove_ task en TaskModel

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 def main():
+    task = TaskModel ("Estudiar para el examen")
+    print (f"Tarea creada: {task.get_task_name()}")
+    task.set_done()
+    print (f"Tarea completada: {task.is_done()}")
+    task.remove_task()
+    print (f"Tarea eliminada: {task.get_task_name()}")
     print("Bienvenido a ToDoApp")
 
 if __name__ == "__main__":
